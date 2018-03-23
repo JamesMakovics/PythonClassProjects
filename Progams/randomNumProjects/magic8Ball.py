18ab5b1a137bee77c8072b6125334034fc2beb2a
James Makovics <jmakovics1@gmail.com>
2018-03-23 08:30:32 -0400

    simple magic 8 ball program

---
+++ Progams/randomNumProjects/magic8Ball.py
@@ -0,0 +1,9 @@
+#Magic 8 Ball
+import random
+
+user_input = input(str("what is your question?: "))
+responses = ["Ask Again Later", "Better Not Tell You Now","No","Outlook Good", "Outlook not so good", "Yes", "Signs point to Yes!", "Yes definitely", "You may rely on it."]
+
+number = random.randint(1,9)
+
+print(responses[number])
