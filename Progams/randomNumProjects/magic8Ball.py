dea7bca49f6a22e3ac51c78bce3ce6255cc8a0d6
James Makovics <jmakovics1@gmail.com>
2018-03-23 14:14:20 -0400

    fixed index out of range in magic8Ball

--- Progams/randomNumProjects/magic8Ball.py
+++ Progams/randomNumProjects/magic8Ball.py
@@ -4,6 +4,6 @@ import random
 user_input = input(str("what is your question?: "))
 responses = ["Ask Again Later", "Better Not Tell You Now","No","Outlook Good", "Outlook not so good", "Yes", "Signs point to Yes!", "Yes definitely", "You may rely on it."]
 
-number = random.randint(0,9)
+number = random.randint(0,8)
 
 print(responses[number])
