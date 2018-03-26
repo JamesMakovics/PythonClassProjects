fbfd5de71bd1bd0c1d0cbd79916ef95fa3f5f289
JamesMakovics <jmakovics1@gmail.com>
2018-03-26 16:28:49 -0400

    Trying pygame class

---
+++ Programs/RaspberryPiRobot/pyGameControl.py
@@ -0,0 +1,82 @@
+#simple control using pygame
+
+'''
+imports
+'''
+import curses
+import RPi.GPIO as GPIO
+import pygame
+GPIO.setmode(GPIO.BCM)
+'''
+Setup of GPIO
+'''
+
+MotorLeftBackward = 17 #in pin 11 on RPI 3
+MotorLeftForward = 27 #in pin 13 on RPI 3
+MotorRightForward = 22 #in pin 15 on RPI 3
+MotorRightBackward = 23 #in pin 16 on RPI 3
+
+'''
+Init the GPIO pins
+'''
+GPIO.setup(MotorLeftForward, GPIO.OUT)
+GPIO.setup(MotorLeftBackward, GPIO.OUT)
+GPIO.setup(MotorRightForward, GPIO.OUT)
+GPIO.setup(MotorRightBackward, GPIO.OUT)
+
+def init(): #sets all the motors to off
+    GPIO.output(MotorLeftForward, False)
+    GPIO.output(MotorRightForward, False)
+    GPIO.output(MotorLeftBackward, False)
+    GPIO.output(MotorRightBackward, False)
+
+def Forward():#makes the robot go forward
+    GPIO.output(MotorLeftForward, True)
+    GPIO.output(MotorRightForward, True)
+    GPIO.output(MotorLeftBackward, False)
+    GPIO.output(MotorRightBackward, False)
+
+def Backward():#makes the robot go backwards
+    GPIO.output(MotorLeftForward, False)
+    GPIO.output(MotorRightForward, False)
+    GPIO.output(MotorLeftBackward, True)
+    GPIO.output(MotorRightBackward, True)
+
+def Left():#makes the robot go left
+    GPIO.output(MotorLeftForward, True)
+    GPIO.output(MotorRightForward, False)
+    GPIO.output(MotorLeftBackward, False)
+    GPIO.output(MotorRightBackward, False)
+
+def Right():#makes the robot go right
+    GPIO.output(MotorLeftForward, False)
+    GPIO.output(MotorRightForward, True)
+    GPIO.output(MotorLeftBackward, False)
+    GPIO.output(MotorRightBackward, False)
+
+def Stop():#makes the robot motors stop
+    GPIO.output(MotorLeftForward, False)
+    GPIO.output(MotorRightForward, False)
+    GPIO.output(MotorLeftBackward, False)
+    GPIO.output(MotorRightBackward, False)
+
+init() #turns motors off from previous run
+
+try:
+    while True:
+        keys = pygame.key.get_pressed()  #checking pressed keys
+        if keys[pygame.K_ESCAPE]: #Quits the program
+            break
+        elif keys[pygame.K_UP]: #Moves Robot forward
+            Forward()
+        elif keys[pygame.K_DOWN]: #Moves Robot Backwards
+            Backward()
+        elif keys[pygame.K_LEFT]:
+            Left()
+        elif keys[pygame.K_RIGHT]:
+            Right()
+        else:
+            Stop()
+finally:
+    #Close program
+    GPIO.cleanup()
