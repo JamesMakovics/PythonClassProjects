95d34519bb197dae3e907ff95ab4bd4e703487a9
James Makovics <jmakovics1@gmail.com>
2018-03-27 08:39:00 -0400

    Added in a better shutdown function for the program.

--- Programs/RaspberryPiRobot/pyGameControl.py
+++ Programs/RaspberryPiRobot/pyGameControl.py
@@ -79,6 +79,8 @@ try:
         for event in events:
             if event.type == pygame.QUIT:
                 GPIO.cleanup()
+                pygame.display.quit()
+                pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE: #Quits the program
                     break
