801194e4533c3a550fbc99006af3daee4a57d523
James Makovics <jmakovics1@gmail.com>
2018-04-10 08:04:42 -0400

    made the print statement a return statement

--- Programs/FunctionChallenges/lastNameFirstMethod.py
+++ Programs/FunctionChallenges/lastNameFirstMethod.py
@@ -1,6 +1,6 @@
 
 def lastNameFirst(lastName, firstName):
-    print("Hello, " + lastName + " " + firstName)
+    return("Hello, " + lastName + " " + firstName)
 
 firstName = str(input("Enter your first name: "))
 lastName = str(input("Enter your last name: "))
