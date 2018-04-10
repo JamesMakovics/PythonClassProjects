40d1525e5bb67bdeaa234612d18b596774220059
James Makovics <jmakovics1@gmail.com>
2018-04-10 08:09:26 -0400

    changes print to a return in salesTaxMethod

--- Programs/FunctionChallenges/salesTaxMethod.py
+++ Programs/FunctionChallenges/salesTaxMethod.py
@@ -1,7 +1,7 @@
 tax = .06
 
 def salesTax(tax, item):
-    print("The item costs: $", item + (item * tax))
+    return("The item costs: $", item + (item * tax))
 
 item = float(input("Enter how much the item was: "))
 salesTax(tax, item)
