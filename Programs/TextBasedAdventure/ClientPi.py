be1544c8495c275bf56ec449930e82a6b9656709
James Makovics <jmakovics1@gmail.com>
2018-04-18 17:29:56 -0400

    Fixed the reference for the string

--- Programs/TextBasedAdventure/ClientPi.py
+++ Programs/TextBasedAdventure/ClientPi.py
@@ -8,7 +8,6 @@ sock.bind((UDP_IP, UDP_PORT))
 while True:
     step, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     time, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    step = int(step)
     time = int(time)
     runMotors.getDriveCommand(step,time)
 
