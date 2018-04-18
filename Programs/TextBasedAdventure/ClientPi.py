6ac178bd314846cd88df4ff9c270c316204426c3
James Makovics <jmakovics1@gmail.com>
2018-04-18 08:20:40 -0400

    added in the randomizer and added a display questions method

--- Programs/TextBasedAdventure/ClientPi.py
+++ Programs/TextBasedAdventure/ClientPi.py
@@ -5,9 +5,9 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 while True:
-data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    step, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    time, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    runMotors.getDriveCommand(step,time)
 
-runMotors.getDriveCommand(step,time)
-
-if runMotors.isFinished() == True:
-    sock.sendto("Finished")
+    if runMotors.isFinished() == True:
+        sock.sendto("Finished")
--- Programs/TextBasedAdventure/mazeGame.py
+++ Programs/TextBasedAdventure/mazeGame.py
@@ -1,22 +1,23 @@
 #This manages the game from the client side
 import pygame
 import socket
+import random
 stepNum = 0
 
-stepsForMaze = []
-motorTimeForCorrectSteps[]
+stepsForMaze = [1,1]
+motorTimeForCorrectSteps = [1,1]
 
-incorrectStepsForMaze = []
-motorTimeForIncorrectSteps = []
+incorrectStepsForMaze = [1,1]
+motorTimeForIncorrectSteps = [1,1]
 
 mathQuestions = ["Whats 9+10?","Find the value of 3x+9=0","What is the factor of 3x^2+6x+3","","",""]
 mathAnswers = [21,3,]
 
-scienceQuestions = []
-scienceAnswers = []
+scienceQuestions = [1,1]
+scienceAnswers = [1,1]
 
-englishQuestions = []
-englishAnswers = []
+englishQuestions = [1,1]
+englishAnswers = [1,1]
 
 
 gameFinished = False
@@ -29,26 +30,39 @@ def runGame():
 
 def intro():
     print("Hello")
+    time.sleep(.5)
     print("Welcome to MazeCraze")
+    time.sleep(.5)
     print("You will need to answer questions in order to move the robot")
+    time.sleep(.5)
     print("You have three topics to choose from:")
     print("Math")
     print("Science")
     print("English")
-    choice = str(input("Please enter a topic: "))
-    choice = choice.lower()
+    while True:
+        choice = str(input("Please enter a topic: "))
+        choice = choice.lower()
+        if choice != "science" or "math" or "english":
+            print("Please enter: \nMath\nScience\nEnglish")
+        else:
+            False
+    displayQuestion(choice,mathQuestions,scienceQuestions,englishQuestions)
+
+def displayQuestion(choice,mathQuestions,scienceQuestions,englishQuestions):
     if choice == "science":
-        displayQuestion()
-    elif choice == "math":
-        displayQuestion()
-    elif choice == "english":
-        displayQuestion()
+        question = scienceQuestions[random.randint(0,26)]
+    if choice == "math":
+        question = mathQuestions[random.randint(0,26)]
+    if choice == "english":
+        question = englishQuestions[random.randint(0,26)]
 
-def displayQuestion():
-    #This method will take a question at random from the list and display it
-    #This will send the send the answer for validation
+    for x in range (0,5):
+        b = "Your question is" + "." * x
+        print (b, end="\r")
+        time.sleep(1)
+    print(question)
 
-def validateQuestion():
+#def validateQuestion():
     #This will take the answer and return if its correct or not
 
 def checkStep(stepsForMaze,incorrectStepsForMaze,motorTimeForCorrectSteps,motorTimeForIncorrectSteps):
@@ -63,15 +77,15 @@ def checkStep(stepsForMaze,incorrectStepsForMaze,motorTimeForCorrectSteps,motorT
         time = motorTimeForIncorrectSteps[stepNum]
         sendMotorMethods()
 
-def sendMotorMethods():
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+def sendMotorMethods(step,time):
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 5005
 
-sock = socket.socket(socket.AF_INET, # Internet
+    sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(step,time)
+    sock.sendto(step)
+    sock.sendto(time)
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
 
-data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-
-if data == True:
-    displayQuestion()
+    if data == "Finished":
+        displayQuestion()
