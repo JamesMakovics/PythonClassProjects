486931827abfdd8417857c33711c9c8a40479d7b
James Makovics <jmakovics1@gmail.com>
2018-04-19 08:49:00 -0400

    removed english questions

--- Programs/TextBasedAdventure/mazeGame.py
+++ Programs/TextBasedAdventure/mazeGame.py
@@ -21,10 +21,6 @@ mathAnswers = ["19","3","2x^2+13x+20","841","11","512","8","1/8","2","552"]
 scienceQuestions = ["What is the closest star to our own sun?","Betelgeuse and Rigel are the two giant stars in which constellation?","In our solar system, which planet has the shortest day?","Which gland in the human body regulates metabolism?","What common kitchen item is made up of sodium and chlorine atoms?","What is the name for the specialized nerve cell that transmits information chemically and electrically throughout the body?","What is the name for the disc-shaped region of icy bodies that extends from Neptune to about 55 astronomical units from the Sun?","In our solar system which two planets are known as ice giants?","Penicillin was discovered in 1928 by which Scottish scientist?","Bronze is an alloy consisting primarily of what two elements?"]
 scienceAnswers = ["Proxima Centauri","Orion","Jupiter","Thyroid","Salt","Neuron","The Kuiper Belt","Uranus and Neptune","Sir Alexander Fleming","Copper and Tin"]
 
-englishQuestions = [1,1]
-englishAnswers = [1,1]
-
-
 gameFinished = False
 
 def runGame():
@@ -44,11 +40,10 @@ def intro():
     print("You have three topics to choose from:")
     print("Math")
     print("Science")
-    print("English")
     while True:
         choice = str(input("Please enter a topic: "))
         print(choice)
-        if choice == "science" or "math" or "english":
+        if choice == "science" or "math":
             break
         else:
             print()
@@ -59,16 +54,13 @@ def intro():
 def displayQuestion(mathQuestions,scienceQuestions,englishQuestions):
     print()
     global choice
-    randNum = random.randint(0,0)
+    randNum = random.randint(0,9)
     if choice == "science":
         question = scienceQuestions[randNum]
         answer = scienceAnswers[randNum]
     if choice == "math":
         question = mathQuestions[randNum]
         answer = mathAnswers[randNum]
-    if choice == "english":
-        question = englishQuestions[randNum]
-        answer = englishAnswers[randNum]
 
     for x in range (0,5):
         b = "Your question is" + "." * x
