842396c8922a0376103c492bba7ff96a38fa4c60
James Makovics <jmakovics1@gmail.com>
2018-04-19 08:42:55 -0400

    added more science questions

--- Programs/TextBasedAdventure/mazeGame.py
+++ Programs/TextBasedAdventure/mazeGame.py
@@ -11,15 +11,15 @@ stepNum = 0
 incorrectCount = 0
 choice = ""
 
-stepsForMaze = ["forward"] #Just a place holder
+stepsForMaze = ["forward","left"] #Just a place holder
 motorTimeForSteps = [1,1] #Just a place holder
 
 
 mathQuestions = ["Whats 9+10?","Find the value of 3x+9=0","Simplify: (x+4)*(2x+5) (Use '^' as an exponent)","What is 29²?","5(4x+11)+3(2x+4)=347\nx=?","What is 8³?","What is the units digit of 8 to the 97th?","2⁻³","1+1=?","46*12=?"]
 mathAnswers = ["19","3","2x^2+13x+20","841","11","512","8","1/8","2","552"]
 
-scienceQuestions = ["What is the closest star to our own sun?","Betelgeuse and Rigel are the two giant stars in which constellation?","In our solar system, which planet has the shortest day?","Which gland in the human body regulates metabolism?","What common kitchen item is made up of sodium and chlorine atoms?","What is the name for the specialized nerve cell that transmits information chemically and electrically throughout the body?","What is the name for the disc-shaped region of icy bodies that extends from Neptune to about 55 astronomical units from the Sun?","In our solar system which two planets are known as ice giants?"]
-scienceAnswers = ["Proxima Centauri","Orion","Jupiter","Thyroid","Salt","Neuron","The Kuiper Belt","Uranus and Neptune"]
+scienceQuestions = ["What is the closest star to our own sun?","Betelgeuse and Rigel are the two giant stars in which constellation?","In our solar system, which planet has the shortest day?","Which gland in the human body regulates metabolism?","What common kitchen item is made up of sodium and chlorine atoms?","What is the name for the specialized nerve cell that transmits information chemically and electrically throughout the body?","What is the name for the disc-shaped region of icy bodies that extends from Neptune to about 55 astronomical units from the Sun?","In our solar system which two planets are known as ice giants?","Penicillin was discovered in 1928 by which Scottish scientist?","Bronze is an alloy consisting primarily of what two elements?"]
+scienceAnswers = ["Proxima Centauri","Orion","Jupiter","Thyroid","Salt","Neuron","The Kuiper Belt","Uranus and Neptune","Sir Alexander Fleming","Copper and Tin"]
 
 englishQuestions = [1,1]
 englishAnswers = [1,1]
