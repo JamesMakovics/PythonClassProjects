057d5a513d903af63ad86c604cd48f1011a605b8
James Makovics <jmakovics1@gmail.com>
2018-05-21 08:50:41 -0400

    Fixed some confusing syntax

--- Vocab/extendAndInsertMethods.py
+++ Vocab/extendAndInsertMethods.py
@@ -1,13 +1,19 @@
 #Extend()
 
+print("Extend Method")
+
 aList = ["OOF","Hello","Lobster"];
 bList = ["Insert","Gabe"];
+
 print("aList : ",aList)
 print("bList : ",bList)
 aList.extend(bList)
 print("Extended List : ", aList)
 
+print("\n \n")
+
 #insert()
+print("Insert Method")
 
 cList = ["Internet", "Explorer", "is", "better", "than"]
 
